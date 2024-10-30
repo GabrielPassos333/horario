8f2afa8edf212e5cafcff18ce0d3a6149e4af831
Gabriel Cruz dos Passos <passosc.gabriel@hotmail.com>
2024-10-30 15:20:53 -0300

    sql

---
+++ src/main/java/piTeste/horario/repository/ProfRepository.java
@@ -0,0 +1,9 @@
+package piTeste.horario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import piTeste.horario.entity.Prof;
+
+public interface ProfRepository extends JpaRepository<Prof,Long>{
+
+}
